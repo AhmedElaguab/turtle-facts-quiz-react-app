a3d11c2011f96651fb90363a94ef3dfb5c8bbaa4
AhmedElaguab <elaguabahmed97@gmail.com>
2019-01-21 21:51:20 +0100

    feat: Add main app header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        {/* Main App Header */}
+        <header>
+          <div className="container">
+            <h1 className="mt-4">Turtle Facts Quiz</h1>
+            <p className="lead">
+              Learn about all the turtles below before you decide to take on the{" "}
+              <strong>TURTLE QUIZ</strong>
+            </p>
+            <hr />
+          </div>
         </header>
       </div>
     );
